530354c968d85575ef82a35e1ece46b49e192186
azharanees <azharanees1@gmail.com>
2019-05-05 22:32:27 +0530

    added json body parser

--- server.js
+++ server.js
@@ -167,6 +167,14 @@ const headers = {
 //     searchTerm = req.params.search;
 //     calculatePrice(searchTerm,res);
 //    });
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
 app.get('/',(req,res)=>{
 
     res.send('Hello World');
@@ -183,7 +191,7 @@ app.get('/api/getprice/:item',(req,res)=>{
 });
 
 app.post('/api/getprediction',(req,res)=>{
-    console.log();
+    console.log(req.body);
    // res.write("getpredictionworking");
     predictPrice(searchTerm,res);
 
