3e8204d6a54ea1aaea0e7eba5a9f4948cf224849
azharanees <azharanees1@gmail.com>
2019-05-09 12:55:09 +0530

    adding recommended products

--- server.js
+++ server.js
@@ -157,6 +157,125 @@ function getItemFromLink(link,res){
 
 
 
+function getRecommendedProducts(searchTerm, res){
+
+
+    var finalJson;
+    let ebay = new eBay({
+        clientID: "pricefin-pricefin-PRD-bf2cd4cf7-bca5e5ee",
+     limit:20,// fetches top 10 results in form of JSON.
+        env: "PRODUCTION" // optional default = "PRODUCTION"
+    })
+    
+   // const searchTerm = "iphone 5s";
+    var jsonRes =[];
+     stri = "";
+    
+
+     var recommededJson = {
+
+     };
+    var prices = 0;
+    var pArr = [];
+    var total  = 0;
+    var img = [];
+    var urlArr = [];
+    
+    ebay.findItemsByKeywords(searchTerm).then((data) => {
+        jsonRes = data;
+        // var item["@count"] = "";
+        
+       // console.log(JSON.stringify(data));
+    var itemCount = 0;
+        for(var searchResult in jsonRes[0].searchResult[0].item){
+          
+
+            prices = (JSON.stringify(jsonRes[0].searchResult[0].item[searchResult].sellingStatus[0].currentPrice[0].__value__));
+
+
+
+            var imgageurl = (JSON.stringify(jsonRes[0].searchResult[0].item[searchResult].galleryURL[0]));
+            var urlArray = (JSON.stringify(jsonRes[0].searchResult[0].item[searchResult].viewItemURL[0]));
+                
+    
+            var fPrice = parseFloat(prices.replace(/"/g,''));
+           // console.log((fPrice));
+            total =fPrice+total;
+            pArr.push(fPrice);
+            img.push(imgageurl);
+            urlArr.push(urlArray);
+            itemCount++;
+            
+    
+    
+        }
+  
+        
+    //    console.log("Total : " +total);
+    //    console.log("items : " +itemCount);
+    //    console.log("Average : " +total/itemCount);
+    //    console.log("Image :" + img.toString());
+    //    console.log("Viewed URL :" + urlArr.toString());
+    //    console.log("Productprice" + pArr)
+       //  predictedPrice = total/itemCount;
+
+         recommededJson =  {
+            'phone':{
+               'img' : img[0],
+               'url' : urlArr[0],
+               'price' : pArr[0]
+            },
+             'phoneOne':{
+                'img' : img[1],
+                'url' : urlArr[1],
+                'price' : pArr[1]
+             },
+             'phoneTwo':{
+                'img' : img[2],
+                'url' : urlArr[2],
+                'price' : pArr[2]
+             },
+             'phoneThree':{
+                'img' : img[3],
+                'url' : urlArr[3],
+                'price' : pArr[3]
+             },
+             'phoneFour':{
+                'img' : img[4],
+                'url' : urlArr[4],
+                'price' : pArr[4]
+             },
+         };
+
+
+         /*result="recommendedJson:[";
+         for(i in img,urlArr,pArr){
+             result=result+'{urlArr:'+i+",img,urlArr,parr"+img[]+'}';
+         }*/
+
+
+         
+   
+
+     
+  /*  return predictedJson;
+    }, (error) => {
+        console.log(error);*/
+   
+    }).then((json)=>{
+        console.log(JSON.stringify(recommededJson));
+        finalJson = json;
+        res.send((recommededJson));
+
+        //return recommededJson;
+      (error) =>{
+            console.log(error)
+        }
+     
+},function(error){
+    console.log(error);
+});
+}
 
 function predictPrice(searchTerm,resp){
 
@@ -206,6 +325,13 @@ const headers = {
 
 }
 
+var searchTerm;
+function setSearchTerm(searchTerm){
+this.searchTerm = searchTerm;
+}
+
+
+
 
 // function (req,res){
 //     console.log('requested url: ' + req.url);
@@ -232,9 +358,14 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 
+app.get('/api/getRecommendedProducts', (req,res)=>{
+
+    getRecommendedProducts(searchTerm,res);
+})
+
 app.get('/',(req,res)=>{
 
-    res.send('Hello World');
+    res.send('Price Finder API');
 
 });
 
@@ -243,6 +374,8 @@ app.get('/api/getprice/:item',(req,res)=>{
         searchTerm = req.params.item;
         console.log(searchTerm);
          calculatePrice(searchTerm,res);
+         setSearchTerm(searchTerm);
+
          
 
 });
@@ -251,6 +384,8 @@ app.post('/api/getprediction',(req,res)=>{
     console.log(req.body);
    // res.write("getpredictionworking");
     predictPrice(searchTerm,res);
+    setSearchTerm(searchTerm);
+
 
 });
 
